52752c3162feefe362868f1e4eb0571a68b86549
mariejonsson <jonsson139marie@gmail.com>
2022-12-18 14:46:40 +0100

    update variable name and description

--- advent_code_2022_1.py
+++ advent_code_2022_1.py
@@ -5,12 +5,16 @@ Find the Elf carrying the most Calories. How many total Calories is that Elf car
 import pandas as pd
 import numpy as np
 
-input_df = pd.read_csv("input_day1", sep=" ", header=None, skip_blank_lines=False)
+input_df = pd.read_csv("input_data/input_day1", sep=" ", header=None, skip_blank_lines=False)
 input_df.columns = ["calories"]
 
-c = input_df['calories'].isna()
-s = c.cumsum()
-df_dict = dict(iter(input_df['calories'].groupby(s)))
+check_nan_bool = input_df['calories'].isna()    # set True or False
+cluster_input = check_nan_bool.cumsum()         # Return the cumulative sum of the elements along a given axis.
+
+df_dict = dict(iter(input_df['calories'].groupby(cluster_input)))
+# iter(obj, sentinel)
+# obj : Object which has to be converted to iterable ( usually an iterator )
+# sentinel : value used to represent end of sequence.
 
 list_key_sum = []
 
